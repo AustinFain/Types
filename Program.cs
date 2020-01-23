c86ae3541bf61924ad40109e19734bddfa00b62e
AustinFain <56362205+AustinFain@users.noreply.github.com>
2020-01-23 10:35:00 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Austin";
+            int grade = 98;
+            char letterGrade = 'A';
+            double costOfTuition = 15000.00;
+
+            Console.WriteLine($"{firstName} has a {grade}% {letterGrade} and a tuition cost of ${costOfTuition}");
+            
+
+        }
+    }
+}
